37ec82aed8e7b388a7e0cc54e29f11b99ec835d6
최경빈 <iris1224@interpark.com>
2023-01-22 19:53:49 +0900

    CNN experiment

--- Pytorch_DL/modules/CIFAR10_experiment.py
+++ Pytorch_DL/modules/CIFAR10_experiment.py
@@ -2,7 +2,9 @@
 - Classification performance test using deep learning architectures
 - Data set : CIFAR10
 - Models : MLP, CNN, VGG, ResNet, MobileNetV2
-- Contents
-    - Accuracy, Loss of test set
-    - Accuracy, Loss by model & hyper-parameters
+- Main function
+    - Model Training
+    - Saving Accuracy, Loss by model & hyper-parameters
+    - Saving files of visualization 
+    - Saving pytorch model (**best params)
 """
